bcb073a0b78d2833018f1c31938dc6f18528b837
FishyTree <53624883+camcartier@users.noreply.github.com>
2023-03-24 11:26:09 +0100

    Shaders

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    #region Sound
+    [SerializeField] AudioSource _menuBackgroundMusic;
+    [SerializeField] AudioSource _meadowBackgroundMusic;
+
+    #endregion
+
+    #region Pause
+    public bool _isPaused;
+    public bool _isPlaying;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
